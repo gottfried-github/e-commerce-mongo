226704bb82e2fe83d3920fe91b1700efd18d719c
osmomysl <dannvx@gmail.com>
2022-11-06 10:18:02 +0200

    auth, controllers, _create: add tests

--- test/auth/auth_controllers.js
+++ test/auth/auth_controllers.js
@@ -1,4 +1,5 @@
 import {assert} from 'chai'
+import {ValidationError, ValueNotUnique} from '../../src/helpers.js'
 
 import {_create} from '../../src/auth/controllers.js'
 
@@ -21,16 +22,35 @@ function testCreate() {
         })
     })
     
-    // describe('create cb returns', () => {
-    //     it('returns the value, returned by create', async () => {
-    //         const res = await _create({}, {
-    //             generateHash: () => {},
-    //             create: async () => {
-    //                 return "some id"
-    //             }
-    //         })
-    //     })
-    // })
+    describe("'create' cb returns", () => {
+        it("returns the value, returned by 'create' cb", async () => {
+            const data = "some data"
+            const res = await _create({}, {
+                generateHash: () => {},
+                create: async () => {
+                    return data
+                }
+            })
+
+            assert.strictEqual(res, data)
+        })
+    })
+
+    describe("'create' cb throws ValidationError", () => {
+        it('calls validate', async () => {
+            let isCalled = false
+            
+            try {
+                await _create({}, {
+                    create: async () => {throw new ValidationError("some message")},
+                    validate: () => {isCalled = true; return "some errors"},
+                    generateHash: () => {}
+                })
+            } catch (e) {}
+
+            assert.strictEqual(isCalled, true)
+        })
+    })
 }
 
 export {testCreate}
