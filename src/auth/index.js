e6c1a1ab033a3aaea719259aac94bd9571bf2933
osmomysl <dannvx@gmail.com>
2022-11-07 09:38:42 +0200

    auth, add facade

---
+++ src/auth/index.js
@@ -0,0 +1,35 @@
+import {generateHash, isEqualHash} from './helpers.js'
+import {validateObjectId} from '../helpers.js'
+
+import {validate} from './validate.js'
+
+import {_storeCreate, _storeUpdate, _storeDelete, _storeGetById, _storeGetByName} from './store.js'
+import {_create, _getById, _getByName} from './controllers.js'
+
+function Auth(c) {
+    function storeCreate(fields) {
+        return _storeCreate(fields, {c})
+    }
+
+    function storeGetById(id) {
+        return _storeGetById(id, {c})
+    }
+
+    function storeGetByName(name) {
+        return _storeGetByName(name, {c})
+    }
+
+    return {
+        create: (id, fields) => {
+            return _create(id, fileds, {create: storeCreate, validate, generateHash})
+        },
+
+        getById: (id) => {
+            return _getById(id, {getById: storeGetById, validateObjectId})
+        },
+
+        getByName: (name, password) => {
+            return _getByName(name, password, {getByName: storeGetByName, isEqualHash})
+        }
+    }
+}
