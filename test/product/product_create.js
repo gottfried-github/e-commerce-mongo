244525a65419061367b2e00eafd863db37e524d7
osmomysl <dannvx@gmail.com>
2023-03-28 21:17:41 +0300

    upd tests

--- test/product/product_create.js
+++ test/product/product_create.js
@@ -7,22 +7,17 @@ import {ValidationError} from '../../src/helpers.js'
 
 function testCreate() {
     describe("create throws a non-ValidationError error", () => {
-        it("throws the error on AND doesn't call any other dependencies", async () => {
-            const validateCalls = []
+        it("throws the error on", async () => {
             const ERR_MSG = "an error message"
 
             try {
                 await _create({}, {
                     create: async () => {throw new Error(ERR_MSG)},
-                    validate: () => {validateCalls.push(null)},
                 })
             } catch(e) {
                 return assert(
                     // error is the one, thrown by update
                     ERR_MSG === e.message
-
-                    // none of the other dependencies has been called
-                    && [validateCalls.length].filter(l => 0 !== l).length === 0
                 )
             }
 
@@ -30,79 +25,29 @@ function testCreate() {
         })
     })
 
-    describe("create throws an ValidationError", () => {
-        it("validate is called with the 'fields' argument", async () => {
-            const fields = "fields"
-            let isEqual = null
-
-            // once getById is called, validate should be called and then _create should throw
+    describe("create throws a ValidationError", () => {
+        it("throws a ValidationError message", async () => {
             try {
-                const res = await _create(fields, {
+                const res = await _create("fields", {
                     create: async () => {throw new ValidationError()},
-                    validate: (_fields) => {
-                        isEqual = fields === _fields
-                    },
                 })
             } catch(e) {
-                return assert.strictEqual(isEqual, true)
+                return assert.strictEqual(e.code, m.ValidationError.code)
             }
 
-            assert.fail()
-        })
-
-        describe("validate returns falsy", () => {
-            it("throws ValidationConflict", async () => {
-                try {
-                    await _create({}, {
-                        create: async () => {throw new ValidationError()},
-                        validate: () => {
-                            return false
-                        },
-                    })
-                } catch(e) {
-                    return assert.instanceOf(e, ValidationConflict)
-                }
-
-                assert.fail()
-            })
-        })
-
-        describe("validate returns truthy", () => {
-            it("throws the returned value, wrapped in ValidationError", async () => {
-                const errors = "errors"
-
-                try {
-                    await _create({}, {
-                        create: async () => {throw new ValidationError()},
-                        validate: () => {
-                            return errors
-                        },
-                    })
-                } catch(e) {
-                    return assert.strictEqual(e.tree, errors)
-                }
-
-                assert.fail()
-            })
+            assert.fail("didn't throw")
         })
     })
 
     describe("create returns a value", () => {
-        it("returns the returned value AND doesn't call the other dependencies", async () => {
-            const validateCalls = []
+        it("returns the returned value", async () => {
             const id = "an id"
 
             const res = await _create({}, {
                 create: async () => {return id},
-                validate: () => {validateCalls.push(null)},
             })
 
-            return assert(
-                id === res
-
-                // none of the other dependencies has been called
-                && [validateCalls.length].filter(l => 0 !== l).length === 0
-            )
+            return assert.strictEqual(res, id)
         })
     })
 }
