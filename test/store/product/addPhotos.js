0534498f511f4a4d44eddd72966b97019b58e95e
gottfried <gottfried.dev@gmail.com>
2023-12-22 17:09:45 +0200

    _storeAddPhotos: passed non-existing product id

--- test/store/product/addPhotos.js
+++ test/store/product/addPhotos.js
@@ -1,14 +1,59 @@
-import {MongoClient} from 'mongodb'
+import {MongoClient, ObjectId} from 'mongodb'
 import {assert} from 'chai'
 
+import {ResourceNotFound} from '../../../../e-commerce-common/messages.js'
 import {_storeAddPhotos} from '../../../src/product/store.js'
 
 export default function addPhotos(client) {
-    console.log('addPhotos')
     describe('addPhotos', () => {
         describe('passed non-existing product id', () => {
             it("throws and doesn't write photos", async () => {
+                const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                await client.connect()
 
+                const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                let res = null
+    
+                const photos = [
+                    {path: '3'},
+                    {path: '4'},
+                    {path: '5'},
+                ]
+
+                try {
+                    res = await _storeAddPhotos(new ObjectId(), photos, {client, product, photo})
+                } catch (e) {
+                    console.log('_storeAddPhotos threw - e:', e)
+                    if (ResourceNotFound.code !== e.code) {
+                        await photo.deleteMany({})
+                        await product.deleteMany({})
+                        await client.close()
+
+                        assert.fail(e, true, 'expected to throw ResourceNotFound, instead, threw a different error')
+                    }
+
+                    const photosWritten = await photo.find({path: {
+                        $in: ['3', '4', '5']
+                    }}).toArray()
+    
+                    console.log('photosWritten:', photosWritten)
+
+                    if (photosWritten.length) {
+                        await photo.deleteMany({})
+                        await product.deleteMany({})
+                        await client.close()
+
+                        assert.fail("should not have written photos to the 'photo' collection, but the photos are written")
+                    }
+
+                    await photo.deleteMany({})
+                    await product.deleteMany({})
+                    await client.close()
+
+                    assert(true)
+                }
             })
         })
 
