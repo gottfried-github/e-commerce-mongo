9b5a7702d6fcf59cecd1bda3f8b3e688dba0ac70
gottfried <gottfried.dev@gmail.com>
2023-12-31 14:52:23 +0200

    test, store, product, addPhotos: passed existing product id - update

--- test/store/product/addPhotos.js
+++ test/store/product/addPhotos.js
@@ -67,48 +67,50 @@ export default function addPhotos(client) {
         })
 
         describe('passed existing product id', () => {
-            it('writes photos to photo, adds them to product.photos_all and returns true', async () => {
+            it('writes photos to photo, adding public, cover and productId fields and returns true', async () => {
                 const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
                 await client.connect()
 
                 const photo = client.db(process.env.APP_DB_NAME).collection('photo')
                 const product = client.db(process.env.APP_DB_NAME).collection('product')
 
-                const photosInitialRes = await photo.insertMany([
-                    {path: '0'},
-                    {path: '1'},
-                    {path: '2'},
-                ])
+                const photos = [
+                    {
+                        pathPublic: '0',
+                        pathLocal: '0',
+                    },
+                    {
+                        pathPublic: '1',
+                        pathLocal: '1',
+                    },
+                    {
+                        pathPublic: '2',
+                        pathLocal: '2',
+                    },
+                ]
     
                 const productRes = await product.insertOne({
                     expose: false,
-                    photos_all: Object.keys(photosInitialRes.insertedIds).map(k => photosInitialRes.insertedIds[k])
                 })
     
                 console.log('productRes:', productRes)
 
                 let res = null
-    
-                const photos = [
-                    {path: '3'},
-                    {path: '4'},
-                    {path: '5'},
-                ]
 
                 try {
-                    res = await _storeAddPhotos(productRes.insertedId, photos, {client, product, photo})
+                    res = await _storeAddPhotos(productRes.insertedId, photos, {product, photo})
                 } catch (e) {
                     console.log('_storeAddPhotos threw - e:', e)
                     assert.fail(e, true, 'expected to return "true", instead, threw the error')
                 }
     
-                const photosWritten = await photo.find({path: {
-                    $in: ['3', '4', '5']
+                const photosWritten = await photo.find({pathPublic: {
+                    $in: ['1', '2', '3']
                 }}).toArray()
 
                 console.log('photosWritten:', photosWritten)
 
-                if (photosWritten.length !== 3) {
+                if (photosWritten.length !== photos.length) {
                     await photo.deleteMany({})
                     await product.deleteMany({})
                     await client.close()
@@ -116,18 +118,18 @@ export default function addPhotos(client) {
                     assert.fail('photos were not written to the photo collection properly')
                 }
 
-                const productWithPhotos = await product.findOne({photos_all: {
-                    $all: photosWritten.map(photo => photo._id)
-                }})
+                for (const photo of photosWritten) {
+                    if (
+                        photo.cover !== false ||
+                        photo.public !== false ||
+                        photo.productId.toString() !== productRes.insertedId.toString()
+                    ) {
+                        await photo.deleteMany({})
+                        await product.deleteMany({})
+                        await client.close()
 
-                console.log('productWithPhotos:', productWithPhotos)
-
-                if (!productWithPhotos) {
-                    await photo.deleteMany({})
-                    await product.deleteMany({})
-                    await client.close()
-
-                    assert.fail('photos were not written to product.photos_all')
+                        assert.fail("inserted photos fields are filled incorrectly")
+                    }
                 }
 
                 await photo.deleteMany({})
