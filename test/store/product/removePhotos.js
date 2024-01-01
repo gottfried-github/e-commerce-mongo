2afd0fd3d9973c71d5ad88458279b19fd46d9c6b
gottfried <gottfried.dev@gmail.com>
2024-01-01 13:50:09 +0200

    test, store, product, removePhotos: pass non-existent product

--- test/store/product/removePhotos.js
+++ test/store/product/removePhotos.js
@@ -7,8 +7,55 @@ import {_storeRemovePhotos} from '../../../src/product/store.js'
 export default function removePhotos() {
     describe("removePhotos", () => {
         describe("pass non-existent product", () => {
-            it("throws ResourceNotFound", () => {
+            it("throws ResourceNotFound", async () => {
+                const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                await client.connect()
 
+                const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                const fakeId = new ObjectId()
+
+                const photos = [
+                    {
+                        productId: fakeId,
+                        pathPublic: '0',
+                        pathLocal: '0',
+                        public: false,
+                        cover: false,
+                    },
+                    {
+                        productId: fakeId,
+                        pathPublic: '1',
+                        pathLocal: '1',
+                        public: false,
+                        cover: false,
+                    },
+                    {
+                        productId: fakeId,
+                        pathPublic: '2',
+                        pathLocal: '2',
+                        public: false,
+                        cover: false,
+                    },
+                ]
+
+                const resPhotos = await photo.insertMany(photos)
+
+                const photosIds = Object.keys(resPhotos.insertedIds).reduce((ids, index) => {
+                    ids[parseInt(index)] = resPhotos.insertedIds[index]
+                    return ids
+                }, [])
+
+                try {
+                    await _storeRemovePhotos(fakeId, photosIds, {client, product, photo})
+                } catch (e) {
+                    await photo.deleteMany({})
+                    await product.deleteMany({})
+                    await client.close()
+
+                    assert(ResourceNotFound.code === e.code)
+                }
             })
         })
 
