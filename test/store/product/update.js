bb1dab7a563ce66db874d3adcedb11989f3be82d
gottfried <gottfried.dev@gmail.com>
2024-01-01 13:23:58 +0200

    test, store, product, update: public photos present, no cover photo

--- test/store/product/update.js
+++ test/store/product/update.js
@@ -95,8 +95,62 @@ export default function update() {
             })
 
             describe("public photos present, no cover photo", () => {
-                it("throws ValidationError", () => {
-                    
+                it("throws ValidationError", async () => {
+                    const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                    await client.connect()
+
+                    const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                    const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                    const resProduct = await product.insertOne({
+                        expose: false
+                    })
+
+                    const photos = [
+                        {
+                            productId: resProduct.insertedId,
+                            pathPublic: '0',
+                            pathLocal: '0',
+                            public: true,
+                            order: 0,
+                            cover: false
+                        },
+                        {
+                            productId: resProduct.insertedId,
+                            pathPublic: '1',
+                            pathLocal: '1',
+                            public: true,
+                            order: 1,
+                            cover: false
+                        },
+                        {
+                            productId: resProduct.insertedId,
+                            pathPublic: '2',
+                            pathLocal: '2',
+                            public: false,
+                            cover: false
+                        },
+                    ]
+
+                    const resPhotos = await photo.insertMany(photos)
+
+                    let resUpdate = null
+
+                    try {
+                        resUpdate = await _storeUpdate(resProduct.insertedId, {
+                            write: {
+                                expose: true
+                            }
+                        }, {product, photo})
+                    } catch (e) {
+                        await photo.deleteMany({})
+                        await product.deleteMany({})
+                        await client.close()
+
+                        return assert.instanceOf(e, ValidationError)
+                    }
+
+                    assert.fail("didn't throw")
                 })
             })
         })
