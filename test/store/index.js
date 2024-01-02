e590b27ee758c1a51a8af5ba85d0327e5c42f772
gottfried <gottfried.dev@gmail.com>
2024-01-02 14:46:10 +0200

    test - store, product, updatePhotosPublicity: passed a photo that doesn't belong to the given product - implement a test; sketch out another test

--- test/store/index.js
+++ test/store/index.js
@@ -1,5 +1,6 @@
 import addPhotos from './product/addPhotos.js'
 import removePhotos from './product/removePhotos.js'
+import updatePhotosPublicity from './product/updatePhotosPublicity.js'
 import update from './product/update.js'
 
 console.log('test/store/index.js')
@@ -7,5 +8,6 @@ console.log('test/store/index.js')
 describe('store', async () => {
     addPhotos()
     removePhotos()
+    updatePhotosPublicity()
     update()
 })
--- test/store/product/updatePhotosPublicity.js
+++ test/store/product/updatePhotosPublicity.js
@@ -8,18 +8,145 @@ export default function updatePhotosPublicity() {
     describe("updatePhotosPublicity", () => {
         describe("passed a non-existent product", () => {
             it("throws ResourceNotFound", () => {
-                
+
             })
         })
 
         describe("passed a photo that doesn't belong to the given product", () => {
-            it("doesn't change the other photos' publicity", () => {
+            it("throws ResourceNotFound", async () => {
+                const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                await client.connect()
 
+                const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                const resProduct = await product.insertOne({expose: false})
+
+                const photos = [
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '0',
+                        pathLocal: '0',
+                        public: false,
+                        cover: false,
+                    },
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '1',
+                        pathLocal: '1',
+                        public: false,
+                        cover: false,
+                    },
+                    {
+                        productId: new ObjectId(),
+                        pathPublic: '2',
+                        pathLocal: '2',
+                        public: false,
+                        cover: false,
+                    },
+                ]
+
+                const resPhotos = await photo.insertMany(photos)
+
+                const photosIds = Object.keys(resPhotos.insertedIds).reduce((ids, index) => {
+                    ids[parseInt(index)] = resPhotos.insertedIds[index]
+                    return ids
+                }, [])
+
+                try {
+                    await _storeUpdatePhotosPublicity(resProduct.insertedId, photosIds.map(photo => ({
+                        id: photo,
+                        public: true
+                    })), {client, product, photo})
+                } catch (e) {
+                    await photo.deleteMany({})
+                    await product.deleteMany({})
+                    await client.close()
+
+                    console.log("_storeUpdatePhotosPublicity threw, error:", e)
+                    return assert(ResourceNotFound.code === e.code)
+                }
+
+                await photo.deleteMany({})
+                await product.deleteMany({})
+                await client.close()
+
+                assert.fail()
             })
 
-            it("throws ResourceNotFound", () => {
+            /*
+            it("doesn't change the other photos' publicity", async () => {
+                const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                await client.connect()
 
+                const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                const resProduct = await product.insertOne({expose: false})
+
+                const photos = [
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '0',
+                        pathLocal: '0',
+                        public: false,
+                        cover: false,
+                    },
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '1',
+                        pathLocal: '1',
+                        public: false,
+                        cover: false,
+                    },
+                    {
+                        productId: new ObjectId(),
+                        pathPublic: '2',
+                        pathLocal: '2',
+                        public: false,
+                        cover: false,
+                    },
+                ]
+
+                const resPhotos = await photo.insertMany(photos)
+
+                const photosIds = Object.keys(resPhotos.insertedIds).reduce((ids, index) => {
+                    ids[parseInt(index)] = resPhotos.insertedIds[index]
+                    return ids
+                }, [])
+
+                try {
+                    await _storeUpdatePhotosPublicity(resProduct.insertedId, photosIds.map(photo => ({
+                        id: photo,
+                        public: true
+                    })), {client, product, photo})
+                } catch (e) {
+                    
+                }
+
+                const photosDocs = await photo.find({
+                    _id: {
+                        $in: photosIds
+                    }
+                }).toArray()
+
+                for (const photo of photosDocs) {
+                    if (photo.public) {
+                        await photo.deleteMany({})
+                        await product.deleteMany({})
+                        await client.close()
+
+                        return assert.fail("the public field of a photo changed")
+                    }
+                }
+
+                await photo.deleteMany({})
+                await product.deleteMany({})
+                await client.close()
+
+                assert(true)
             })
+            */
         })
 
         describe("passed a public photo that's already public", () => {
