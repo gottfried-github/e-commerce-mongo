578669d5a81d1300b410ae9b4e7d1d4f7ef39aab
gottfried <gottfried.dev@gmail.com>
2024-01-03 16:38:39 +0200

    test - store, product, reorderPhotos: passed a photo that doesn't belong to the given product

--- test/store/product/reorderPhotos.js
+++ test/store/product/reorderPhotos.js
@@ -85,9 +85,79 @@ export default function reorderPhotos() {
             })
         })
 
-        desribe("passed a photo that doesn't belong to the given product", () => {
-            it("throws ResourceNotFound", () => {
+        describe("passed a photo that doesn't belong to the given product", () => {
+            it("throws ResourceNotFound", async () => {
+                const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                await client.connect()
 
+                const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                const resProduct = await product.insertOne({expose: false})
+
+                const fakeId = new ObjectId()
+
+                const photos = [
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '0',
+                        pathLocal: '0',
+                        public: true,
+                        order: 0,
+                        cover: false,
+                    },
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '1',
+                        pathLocal: '1',
+                        public: true,
+                        order: 1,
+                        cover: false,
+                    },
+                    {
+                        productId: fakeId,
+                        pathPublic: '2',
+                        pathLocal: '2',
+                        public: true,
+                        order: 2,
+                        cover: true,
+                    },
+                ]
+
+                const resPhotos = await photo.insertMany(photos)
+
+                const photosIds = Object.keys(resPhotos.insertedIds).reduce((ids, index) => {
+                    ids[parseInt(index)] = resPhotos.insertedIds[index]
+                    return ids
+                }, [])
+
+                const photosData = [
+                    {
+                        id: photosIds[0],
+                        order: 2,
+                    },
+                    {
+                        id: photosIds[2],
+                        order: 0,
+                    },
+                ]
+
+                try {
+                    await _storeReorderPhotos(resProduct.insertedId, photosData, {client, product, photo})
+                } catch (e) {
+                    await photo.deleteMany({})
+                    await product.deleteMany({})
+                    await client.close()
+
+                    console.log("_storeUpdatePhotosPublicity threw, error:", e)
+                    return assert(ResourceNotFound.code === e.code)
+                }
+
+                await photo.deleteMany({})
+                await product.deleteMany({})
+                await client.close()
+
+                assert.fail("didn't throw")
             })
 
             it("doesn't modify any of the photos", () => {
