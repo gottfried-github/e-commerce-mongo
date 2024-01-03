7aad4eb7a1758ac7e98f1d542419cd25b529eec2
gottfried <gottfried.dev@gmail.com>
2024-01-03 16:43:06 +0200

    test - store, product, reorderPhotos: passed a photo that doesn't belong to the given product

--- test/store/product/reorderPhotos.js
+++ test/store/product/reorderPhotos.js
@@ -160,7 +160,82 @@ export default function reorderPhotos() {
                 assert.fail("didn't throw")
             })
 
-            it("doesn't modify any of the photos", () => {
+            it("doesn't modify any of the photos", async () => {
+                const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                await client.connect()
+
+                const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                const resProduct = await product.insertOne({expose: false})
+
+                const fakeId = new ObjectId()
+
+                const photos = [
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '0',
+                        pathLocal: '0',
+                        public: true,
+                        order: 0,
+                        cover: false,
+                    },
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '1',
+                        pathLocal: '1',
+                        public: true,
+                        order: 1,
+                        cover: false,
+                    },
+                    {
+                        productId: fakeId,
+                        pathPublic: '2',
+                        pathLocal: '2',
+                        public: true,
+                        order: 2,
+                        cover: true,
+                    },
+                ]
+
+                const resPhotos = await photo.insertMany(photos)
+
+                const photosIds = Object.keys(resPhotos.insertedIds).reduce((ids, index) => {
+                    ids[parseInt(index)] = resPhotos.insertedIds[index]
+                    return ids
+                }, [])
+
+                const photosData = [
+                    {
+                        id: photosIds[0],
+                        order: 2,
+                    },
+                    {
+                        id: photosIds[2],
+                        order: 0,
+                    },
+                ]
+
+                try {
+                    await _storeReorderPhotos(resProduct.insertedId, photosData, {client, product, photo})
+                } catch (e) {
+                    // expected to throw
+                }
+
+                const photoDoc = await photo.findOne({
+                    _id: new ObjectId(photosData[0].id)
+                })
+
+                await photo.deleteMany({})
+                await product.deleteMany({})
+                await client.close()
+
+                assert(photoDoc.order === 0)
+            })
+        })
+
+        describe("passed a photo that is not public in the database", () => {
+            it("throws ResourceNotFound", () => {
 
             })
         })
