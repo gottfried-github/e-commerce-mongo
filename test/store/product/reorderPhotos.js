c82e90f9ee29771491f5bbe2715c4437a632ccdb
gottfried <gottfried.dev@gmail.com>
2024-01-03 16:49:42 +0200

    test - store, product, reorderPhotos: length of passed photos is not the same as the number of existing photos

--- test/store/product/reorderPhotos.js
+++ test/store/product/reorderPhotos.js
@@ -308,8 +308,76 @@ export default function reorderPhotos() {
         })
 
         describe("length of passed photos is not the same as the number of existing photos", () => {
-            it("throws ValidationError", () => {
+            it("throws ValidationError", async () => {
+                const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                await client.connect()
 
+                const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                const resProduct = await product.insertOne({expose: false})
+
+                const photos = [
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '0',
+                        pathLocal: '0',
+                        public: true,
+                        order: 0,
+                        cover: false,
+                    },
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '1',
+                        pathLocal: '1',
+                        public: true,
+                        order: 1,
+                        cover: false,
+                    },
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '2',
+                        pathLocal: '2',
+                        public: true,
+                        order: 2,
+                        cover: true,
+                    },
+                ]
+
+                const resPhotos = await photo.insertMany(photos)
+
+                const photosIds = Object.keys(resPhotos.insertedIds).reduce((ids, index) => {
+                    ids[parseInt(index)] = resPhotos.insertedIds[index]
+                    return ids
+                }, [])
+
+                const photosData = [
+                    {
+                        id: photosIds[0],
+                        order: 1,
+                    },
+                    {
+                        id: photosIds[1],
+                        order: 0,
+                    },
+                ]
+
+                try {
+                    await _storeReorderPhotos(resProduct.insertedId, photosData, {client, product, photo})
+                } catch (e) {
+                    await photo.deleteMany({})
+                    await product.deleteMany({})
+                    await client.close()
+
+                    console.log("_storeReorderPhotos threw, error:", e)
+                    return assert.instanceOf(e, ValidationError)
+                }
+
+                await photo.deleteMany({})
+                await product.deleteMany({})
+                await client.close()
+
+                assert.fail("didn't throw")
             })
         })
 
