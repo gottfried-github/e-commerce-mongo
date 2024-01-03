a616e14d0d50ae562a8a82c4d3125cb4e7cea356
gottfried <gottfried.dev@gmail.com>
2024-01-03 16:15:06 +0200

    test - store, product, reorderPhotos: given non-existent product

--- test/store/index.js
+++ test/store/index.js
@@ -2,6 +2,7 @@ import addPhotos from './product/addPhotos.js'
 import removePhotos from './product/removePhotos.js'
 import updatePhotosPublicity from './product/updatePhotosPublicity.js'
 import setCoverPhoto from './product/setCoverPhoto.js'
+import reorderPhotos from './product/reorderPhotos.js'
 import update from './product/update.js'
 
 console.log('test/store/index.js')
@@ -11,5 +12,6 @@ describe('store', async () => {
     removePhotos()
     updatePhotosPublicity()
     setCoverPhoto()
+    reorderPhotos()
     update()
 })
--- test/store/product/reorderPhotos.js
+++ test/store/product/reorderPhotos.js
@@ -3,13 +3,85 @@ import {assert} from 'chai'
 
 import {ResourceNotFound} from '../../../../e-commerce-common/messages.js'
 import {ValidationError} from '../../../src/helpers.js'
-import {_storeSetCoverPhoto} from '../../../src/product/store.js'
+import {_storeReorderPhotos} from '../../../src/product/store.js'
 
 export default function reorderPhotos() {
     describe("reorderPhotos", () => {
         describe("passed a non-existent product", () => {
-            it("throws ResourceNotFound", () => {
+            it("throws ResourceNotFound", async () => {
+                const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                await client.connect()
 
+                const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                const fakeId = new ObjectId()
+
+                const photos = [
+                    {
+                        productId: fakeId,
+                        pathPublic: '0',
+                        pathLocal: '0',
+                        public: true,
+                        order: 0,
+                        cover: false,
+                    },
+                    {
+                        productId: fakeId,
+                        pathPublic: '1',
+                        pathLocal: '1',
+                        public: true,
+                        order: 1,
+                        cover: false,
+                    },
+                    {
+                        productId: fakeId,
+                        pathPublic: '2',
+                        pathLocal: '2',
+                        public: true,
+                        order: 2,
+                        cover: false,
+                    },
+                ]
+
+                const resPhotos = await photo.insertMany(photos)
+
+                const photosIds = Object.keys(resPhotos.insertedIds).reduce((ids, index) => {
+                    ids[parseInt(index)] = resPhotos.insertedIds[index]
+                    return ids
+                }, [])
+
+                const photosData = [
+                    {
+                        id: photosIds[0],
+                        order: 2
+                    },
+                    {
+                        id: photosIds[1],
+                        order: 1
+                    },
+                    {
+                        id: photosIds[0],
+                        order: 0
+                    },
+                ]
+
+                try {
+                    await _storeReorderPhotos(new ObjectId(), photosData, {client, product, photoC: photo})
+                } catch (e) {
+                    await photo.deleteMany({})
+                    await product.deleteMany({})
+                    await client.close()
+
+                    console.log("_storeUpdatePhotosPublicity threw, error:", e)
+                    return assert(ResourceNotFound.code === e.code)
+                }
+
+                await photo.deleteMany({})
+                await product.deleteMany({})
+                await client.close()
+
+                assert.fail("didn't throw")
             })
         })
 
