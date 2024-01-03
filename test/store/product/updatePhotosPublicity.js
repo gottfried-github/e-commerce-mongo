101bf683189603bba8165b693e0cfa2b2d92e49c
gottfried <gottfried.dev@gmail.com>
2024-01-03 15:04:14 +0200

    test - store, product, updatePhotosPublicity: sets all currently public photos of an exposed product to false

--- test/store/product/updatePhotosPublicity.js
+++ test/store/product/updatePhotosPublicity.js
@@ -459,8 +459,89 @@ export default function updatePhotosPublicity() {
         })
 
         describe("sets all currently public photos of an exposed product to false", () => {
-            it("sets the expose field on the product to false", () => {
+            it("sets the expose field on the product to false", async () => {
+                const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
+                await client.connect()
 
+                const photo = client.db(process.env.APP_DB_NAME).collection('photo')
+                const product = client.db(process.env.APP_DB_NAME).collection('product')
+
+                const resProduct = await product.insertOne({
+                    name: 'product',
+                    price: 10000,
+                    time: new Date(),
+                    is_in_stock: false,
+                    description: 'some description',
+                    expose: true
+                })
+
+                const photos = [
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '0',
+                        pathLocal: '0',
+                        public: true,
+                        order: 0,
+                        cover: false,
+                    },
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '1',
+                        pathLocal: '1',
+                        public: true,
+                        order: 1,
+                        cover: false,
+                    },
+                    {
+                        productId: resProduct.insertedId,
+                        pathPublic: '2',
+                        pathLocal: '2',
+                        public: true,
+                        order: 2,
+                        cover: true,
+                    },
+                ]
+
+                const resPhotos = await photo.insertMany(photos)
+
+                const photosIds = Object.keys(resPhotos.insertedIds).reduce((ids, index) => {
+                    ids[parseInt(index)] = resPhotos.insertedIds[index]
+                    return ids
+                }, [])
+
+                const photosData = [
+                    {
+                        id: photosIds[0],
+                        public: false,
+                    },
+                    {
+                        id: photosIds[1],
+                        public: false,
+                    },
+                    {
+                        id: photosIds[2],
+                        public: false
+                    }
+                ]
+
+                try {
+                    await _storeUpdatePhotosPublicity(resProduct.insertedId, photosData, {client, product, photo})
+                } catch (e) {
+                    await photo.deleteMany({})
+                    await product.deleteMany({})
+                    await client.close()
+
+                    console.log("_storeUpdatePhotosPublicity threw, error:", e)
+                    return assert.fail("threw an error")
+                }
+
+                const productDoc = await product.findOne({_id: resProduct.insertedId})
+
+                await photo.deleteMany({})
+                await product.deleteMany({})
+                await client.close()
+
+                assert(!productDoc.expose)
             })
         })
     })
